baddc587bd356eeb393b06230ea52bc2dbf75667
MANOJ PATRA <patra.manoj0@gmail.com>
2017-11-27 00:17:13 +0530

    Add services to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { GeolocationService } from './services/geolocation.service';
+import { DataService } from './services/data.service';
 import { AppComponent } from './app.component';
 
 
@@ -11,7 +12,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    GeolocationService,
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
