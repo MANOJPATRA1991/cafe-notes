b9ccb798c8f45ec6e98a738155aa6f1cd961dd27
MANOJ PATRA <patra.manoj0@gmail.com>
2017-11-27 00:02:12 +0530

    Add geo location service

---
+++ src/app/services/geolocation.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class GeolocationService {
+
+  constructor() { }
+
+  /**
+   * Request user locatiion
+   * @param {function} callback : Handle response
+   */
+  requestLocation(callback) {
+
+  }
+
+  /**
+   * Get the location and 
+   * open a map native app to show the location
+   * @param location : The location of the coffee shop 
+   */
+  getMapLink(location) {
+
+  }
+
+}
