f82204e6a25cde3c2a2593aa0e523268b442e7c4
MANOJ PATRA <patra.manoj0@gmail.com>
2017-11-28 20:32:41 +0530

    Add service to read and write data

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,4 +5,25 @@ export class DataService {
 
   constructor() { }
 
+  /**
+   * Get list of coffees
+   * @param {function} callback : Handle response
+   */
+  getList(callback) {
+    // TODO: Change it to a real coffee list
+    const list = [
+      new Coffee("Double Espresso", "Sunny Cafe", new PlaceLocation("123 Market St", "San Fransisco")),
+      new Coffee("Caramel Americano", "Starcoffee", new PlaceLocation("Gran via 34", "Madrid"))
+    ];
+  }
+
+  /**
+   * Add coffee to the list
+   * @param {Coffee} coffee : Coffee details 
+   * @param {function} callback : Handle response 
+   */
+  save(coffee, callback) {
+    // TODO: Change it with a Real Web Service
+    callback(true);
+  }
 }
