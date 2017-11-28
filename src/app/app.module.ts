33181ddb1a05c5dbed367cd93ff29425106063ce
MANOJ PATRA <patra.manoj0@gmail.com>
2017-11-28 20:49:24 +0530

    Import material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,34 @@ import { GeolocationService } from './services/geolocation.service';
 import { DataService } from './services/data.service';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { 
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatSlideToggleModule
+} from '@angular/material';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSlideToggleModule
   ],
   providers: [
     GeolocationService,
