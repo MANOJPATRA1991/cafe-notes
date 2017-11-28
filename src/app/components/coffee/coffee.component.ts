7ab74083c755d7b1caf5101c33d86b16cf092b43
MANOJ PATRA <patra.manoj0@gmail.com>
2017-11-28 22:02:05 +0530

    Subscribe to id parameter

--- src/app/components/coffee/coffee.component.ts
+++ src/app/components/coffee/coffee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-coffee',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoffeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+
+  routingSubscription: any;
 
   ngOnInit() {
+    // Subscribe to route parameters
+    this.routingSubscription = this.route.params.subscribe(params => {
+      console.log(params["id"]);
+    })
+  }
+
+  ngOnDestroy() {
+    this.routingSubscription.unsubscibe();
   }
 
 }
