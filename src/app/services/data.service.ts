0eda3a560d8cf20387436fd552a6c56bff5239df
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-06 01:22:17 +0530

    Update data service to use firebase

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,25 +2,37 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Coffee } from '../logic/Coffee';
 import { PlaceLocation } from '../logic/PlaceLocation';
+import { 
+  AngularFireDatabase,  
+  AngularFireList
+} from 'angularfire2/database';
 
 @Injectable()
 export class DataService {
 
-  constructor(private http: Http) { }
+  constructor(
+    private http: Http, 
+    private fbdatabase: AngularFireDatabase) { }
 
   // BASE URL
   public endpoint = "http://localhost:3000";
 
+  coffeeList: AngularFireList<any>;
+  
   /**
    * Get coffee details by id
    * @param {string} coffeeId 
    * @param {function} callback
    */
   getCoffee(coffeeId: string, callback) {
-    this.http.get(`${this.endpoint}/coffees/${coffeeId}`)
-    .subscribe(response => {
-      callback(response.json());
-    })
+    this.fbdatabase.database.ref(`/coffees/${coffeeId}`)
+    .on('value', snapshot => {
+      callback(snapshot.val());
+    });
+    // this.http.get(`${this.endpoint}/coffees/${coffeeId}`)
+    // .subscribe(response => {
+    //   callback(response.json());
+    // });
   }
 
   /**
@@ -28,11 +40,16 @@ export class DataService {
    * @param {function} callback : Handle response
    */
   getList(callback) {
-    this.http.get(`${this.endpoint}/coffees`)
-    .subscribe(response => {
-      console.log(response.json());
-      callback(response.json());
-    })
+    this.coffeeList = this.fbdatabase.list('coffees');
+    this.coffeeList
+    .snapshotChanges().subscribe(coffees => {
+      callback(coffees);
+    });
+    // this.http.get(`${this.endpoint}/coffees`)
+    // .subscribe(response => {
+    //   console.log(response.json());
+    //   callback(response.json());
+    // })
   }
 
   /**
@@ -41,18 +58,26 @@ export class DataService {
    * @param {function} callback : Handle response 
    */
   save(coffee, callback) {
+    // It's an update
     if(coffee._id) {
-      // It's an update
-      this.http.put(`${this.endpoint}/coffees/${coffee._id}`, coffee)
-      .subscribe(response => {
+      this.coffeeList.update(coffee._id, coffee)
+      .then(response => {
         callback(true);
-      }) 
+      });
+      // this.http.put(`${this.endpoint}/coffees/${coffee._id}`, coffee)
+      // .subscribe(response => {
+      //   callback(true);
+      // }) 
     } else {
       // It's an insert
-      this.http.post(`${this.endpoint}/coffees`, coffee)
-      .subscribe(response => {
+      this.coffeeList.push(coffee)
+      .then(response => {
         callback(true);
-      }) 
+      });
+      // this.http.post(`${this.endpoint}/coffees`, coffee)
+      // .subscribe(response => {
+      //   callback(true);
+      // }) 
     }
   }
 
@@ -62,9 +87,42 @@ export class DataService {
    * @param {function} callback : Handle response 
    */
   delete(coffee, callback) {
-    this.http.delete(`${this.endpoint}/coffees/${coffee._id}`)
-    .subscribe(response => {
+    this.coffeeList.remove(coffee._id)
+    .then(response => {
       callback(true);
-    })
+    });
+    // this.http.delete(`${this.endpoint}/coffees/${coffee._id}`)
+    // .subscribe(response => {
+    //   callback(true);
+    // })
+  }
+
+  /**
+   * Add push notifications
+   */
+  pushNotification() {
+    // Push notification on coffee update
+    this.fbdatabase.database.ref('coffees')
+    .on("child_changed", (snapshot) => {
+      let updatedCoffee = snapshot.val();
+      let options = {
+        body: `${updatedCoffee.name} details are updated`,
+        icon: "../../icons/icon_96.png"
+      }
+      let n = new Notification("Update", options);
+      setTimeout(n.close.bind(n), 4000);
+    });
+
+    // Push notification when new coffee is added
+    this.fbdatabase.database.ref('coffees').limitToLast(1)
+    .on("child_added", (snapshot) => {
+      let updatedCoffee = snapshot.val();
+      let options = {
+        body: `${updatedCoffee.name} is added to coffee list`,
+        icon: "../../icons/icon_96.png"
+      }
+      let n = new Notification("New coffee added", options);
+      setTimeout(n.close.bind(n), 4000);
+    });
   }
 }
