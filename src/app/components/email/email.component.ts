aa09cf2dea541196a7403d6b6c57a069a152d497
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-08 00:09:18 +0530

    Update email component

--- src/app/components/email/email.component.ts
+++ src/app/components/email/email.component.ts
@@ -1,13 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { UserserviceService } from '../../services/userservice.service';
+
+import {moveIn, fallIn} from '../router.animations';
+
+import { User } from '../../logic/User';
 
 @Component({
   selector: 'app-email',
   templateUrl: './email.component.html',
-  styleUrls: ['./email.component.css']
+  styleUrls: ['./email.component.css'],
+  animations: [moveIn(), fallIn()],
+  host: {'[@moveIn]': ''}
 })
+
 export class EmailComponent implements OnInit {
 
-  constructor() { }
+  state: string = '';
+  error: any;
+  user: User;
+
+  constructor(
+    public fbAuth: AngularFireAuth,
+    private router: Router,
+    private userService: UserserviceService
+  ) { 
+    this.fbAuth.authState.subscribe(user => {
+      if(user) {
+        this.router.navigate(["/list"]);
+      }
+    })
+  }
+
+  /**
+   * Log in user with email and password
+   */
+  login() {
+    this.userService.login(this.user, result => {
+      if(result === true) {
+        this.router.navigate(["/list"]);
+      } else {
+        this.error = result;
+      }
+    })
+  }
 
   ngOnInit() {
   }
